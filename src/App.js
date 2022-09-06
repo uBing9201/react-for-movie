9ed4da9f75e6bb2fbd1cbc2d95a968e3a4bfabca
yBean0101 <jeonyb0101@naver.com>
2022-09-06 10:25:27 +0900

    part1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  // const getMovies = async() => {
+  //   const response = await fetch(
+  //     `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+  //   );
+  //   const json = await response.json();
+  //   setMovies(json.data.movies);
+  //   setLoading(false);
+  // };
+
+  const getMovies = async() => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  console.log(movies);
+
+  return(
+    <div>
+      {loading ? <h1>Loading...</h1> : <div>{movies.map()}</div>}
     </div>
-  );
+  )
 }
 
 export default App;
